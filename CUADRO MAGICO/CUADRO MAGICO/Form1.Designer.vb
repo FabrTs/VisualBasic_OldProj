3434048744f4d5a175d3624b3e03a25e24384db0
Fabricio Torres <99437781+FabrTs@users.noreply.github.com>
2025-08-07 12:03:31 -0600

    Add files via upload

---
+++ CUADRO MAGICO/CUADRO MAGICO/Form1.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.e1 = New System.Windows.Forms.Label
+        Me.gb2 = New System.Windows.Forms.GroupBox
+        Me.l9 = New System.Windows.Forms.Label
+        Me.l6 = New System.Windows.Forms.Label
+        Me.l8 = New System.Windows.Forms.Label
+        Me.l5 = New System.Windows.Forms.Label
+        Me.l7 = New System.Windows.Forms.Label
+        Me.l4 = New System.Windows.Forms.Label
+        Me.l1 = New System.Windows.Forms.Label
+        Me.l3 = New System.Windows.Forms.Label
+        Me.l2 = New System.Windows.Forms.Label
+        Me.e2 = New System.Windows.Forms.Label
+        Me.e7 = New System.Windows.Forms.Label
+        Me.e4 = New System.Windows.Forms.Label
+        Me.e5 = New System.Windows.Forms.Label
+        Me.e8 = New System.Windows.Forms.Label
+        Me.e3 = New System.Windows.Forms.Label
+        Me.e6 = New System.Windows.Forms.Label
+        Me.e9 = New System.Windows.Forms.Label
+        Me.gb1 = New System.Windows.Forms.GroupBox
+        Me.gb2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'e1
+        '
+        Me.e1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.e1.Location = New System.Drawing.Point(122, 47)
+        Me.e1.Name = "e1"
+        Me.e1.Size = New System.Drawing.Size(39, 23)
+        Me.e1.TabIndex = 0
+        '
+        'gb2
+        '
+        Me.gb2.Controls.Add(Me.l9)
+        Me.gb2.Controls.Add(Me.l6)
+        Me.gb2.Controls.Add(Me.l8)
+        Me.gb2.Controls.Add(Me.l5)
+        Me.gb2.Controls.Add(Me.l7)
+        Me.gb2.Controls.Add(Me.l4)
+        Me.gb2.Controls.Add(Me.l1)
+        Me.gb2.Controls.Add(Me.l3)
+        Me.gb2.Controls.Add(Me.l2)
+        Me.gb2.Location = New System.Drawing.Point(53, 182)
+        Me.gb2.Name = "gb2"
+        Me.gb2.Size = New System.Drawing.Size(310, 80)
+        Me.gb2.TabIndex = 9
+        Me.gb2.TabStop = False
+        Me.gb2.Text = "gb2"
+        '
+        'l9
+        '
+        Me.l9.AutoSize = True
+        Me.l9.Location = New System.Drawing.Point(272, 25)
+        Me.l9.Name = "l9"
+        Me.l9.Size = New System.Drawing.Size(13, 13)
+        Me.l9.TabIndex = 21
+        Me.l9.Text = "9"
+        '
+        'l6
+        '
+        Me.l6.AutoSize = True
+        Me.l6.Location = New System.Drawing.Point(215, 25)
+        Me.l6.Name = "l6"
+        Me.l6.Size = New System.Drawing.Size(13, 13)
+        Me.l6.TabIndex = 19
+        Me.l6.Text = "6"
+        '
+        'l8
+        '
+        Me.l8.AutoSize = True
+        Me.l8.Location = New System.Drawing.Point(253, 25)
+        Me.l8.Name = "l8"
+        Me.l8.Size = New System.Drawing.Size(13, 13)
+        Me.l8.TabIndex = 20
+        Me.l8.Text = "8"
+        '
+        'l5
+        '
+        Me.l5.AutoSize = True
+        Me.l5.Location = New System.Drawing.Point(196, 25)
+        Me.l5.Name = "l5"
+        Me.l5.Size = New System.Drawing.Size(13, 13)
+        Me.l5.TabIndex = 22
+        Me.l5.Text = "5"
+        '
+        'l7
+        '
+        Me.l7.AutoSize = True
+        Me.l7.Location = New System.Drawing.Point(234, 25)
+        Me.l7.Name = "l7"
+        Me.l7.Size = New System.Drawing.Size(13, 13)
+        Me.l7.TabIndex = 19
+        Me.l7.Text = "7"
+        '
+        'l4
+        '
+        Me.l4.AutoSize = True
+        Me.l4.Location = New System.Drawing.Point(151, 25)
+        Me.l4.Name = "l4"
+        Me.l4.Size = New System.Drawing.Size(13, 13)
+        Me.l4.TabIndex = 21
+        Me.l4.Text = "4"
+        '
+        'l1
+        '
+        Me.l1.AutoSize = True
+        Me.l1.Location = New System.Drawing.Point(16, 25)
+        Me.l1.Name = "l1"
+        Me.l1.Size = New System.Drawing.Size(13, 13)
+        Me.l1.TabIndex = 18
+        Me.l1.Text = "1"
+        '
+        'l3
+        '
+        Me.l3.AutoSize = True
+        Me.l3.Location = New System.Drawing.Point(106, 25)
+        Me.l3.Name = "l3"
+        Me.l3.Size = New System.Drawing.Size(13, 13)
+        Me.l3.TabIndex = 20
+        Me.l3.Text = "3"
+        '
+        'l2
+        '
+        Me.l2.AutoSize = True
+        Me.l2.Location = New System.Drawing.Point(61, 25)
+        Me.l2.Name = "l2"
+        Me.l2.Size = New System.Drawing.Size(13, 13)
+        Me.l2.TabIndex = 19
+        Me.l2.Text = "2"
+        '
+        'e2
+        '
+        Me.e2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.e2.Location = New System.Drawing.Point(184, 47)
+        Me.e2.Name = "e2"
+        Me.e2.Size = New System.Drawing.Size(39, 23)
+        Me.e2.TabIndex = 10
+        '
+        'e7
+        '
+        Me.e7.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.e7.Location = New System.Drawing.Point(124, 127)
+        Me.e7.Name = "e7"
+        Me.e7.Size = New System.Drawing.Size(39, 23)
+        Me.e7.TabIndex = 11
+        '
+        'e4
+        '
+        Me.e4.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.e4.Location = New System.Drawing.Point(122, 87)
+        Me.e4.Name = "e4"
+        Me.e4.Size = New System.Drawing.Size(39, 23)
+        Me.e4.TabIndex = 12
+        '
+        'e5
+        '
+        Me.e5.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.e5.Location = New System.Drawing.Point(184, 87)
+        Me.e5.Name = "e5"
+        Me.e5.Size = New System.Drawing.Size(39, 23)
+        Me.e5.TabIndex = 13
+        '
+        'e8
+        '
+        Me.e8.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.e8.Location = New System.Drawing.Point(184, 127)
+        Me.e8.Name = "e8"
+        Me.e8.Size = New System.Drawing.Size(39, 23)
+        Me.e8.TabIndex = 14
+        '
+        'e3
+        '
+        Me.e3.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.e3.Location = New System.Drawing.Point(244, 47)
+        Me.e3.Name = "e3"
+        Me.e3.Size = New System.Drawing.Size(39, 23)
+        Me.e3.TabIndex = 15
+        '
+        'e6
+        '
+        Me.e6.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.e6.Location = New System.Drawing.Point(244, 87)
+        Me.e6.Name = "e6"
+        Me.e6.Size = New System.Drawing.Size(39, 23)
+        Me.e6.TabIndex = 16
+        '
+        'e9
+        '
+        Me.e9.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.e9.Location = New System.Drawing.Point(244, 127)
+        Me.e9.Name = "e9"
+        Me.e9.Size = New System.Drawing.Size(39, 23)
+        Me.e9.TabIndex = 17
+        '
+        'gb1
+        '
+        Me.gb1.Location = New System.Drawing.Point(100, 12)
+        Me.gb1.Name = "gb1"
+        Me.gb1.Size = New System.Drawing.Size(200, 151)
+        Me.gb1.TabIndex = 18
+        Me.gb1.TabStop = False
+        Me.gb1.Text = "GroupBox1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(520, 295)
+        Me.Controls.Add(Me.e9)
+        Me.Controls.Add(Me.e6)
+        Me.Controls.Add(Me.e3)
+        Me.Controls.Add(Me.e8)
+        Me.Controls.Add(Me.e5)
+        Me.Controls.Add(Me.e4)
+        Me.Controls.Add(Me.e7)
+        Me.Controls.Add(Me.e2)
+        Me.Controls.Add(Me.e1)
+        Me.Controls.Add(Me.gb2)
+        Me.Controls.Add(Me.gb1)
+        Me.Name = "Form1"
+        Me.Text = "CUADRO MAGICO"
+        Me.gb2.ResumeLayout(False)
+        Me.gb2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents e1 As System.Windows.Forms.Label
+    Friend WithEvents gb2 As System.Windows.Forms.GroupBox
+    Friend WithEvents e2 As System.Windows.Forms.Label
+    Friend WithEvents e7 As System.Windows.Forms.Label
+    Friend WithEvents e4 As System.Windows.Forms.Label
+    Friend WithEvents e5 As System.Windows.Forms.Label
+    Friend WithEvents e8 As System.Windows.Forms.Label
+    Friend WithEvents e3 As System.Windows.Forms.Label
+    Friend WithEvents e6 As System.Windows.Forms.Label
+    Friend WithEvents e9 As System.Windows.Forms.Label
+    Friend WithEvents l9 As System.Windows.Forms.Label
+    Friend WithEvents l6 As System.Windows.Forms.Label
+    Friend WithEvents l8 As System.Windows.Forms.Label
+    Friend WithEvents l5 As System.Windows.Forms.Label
+    Friend WithEvents l7 As System.Windows.Forms.Label
+    Friend WithEvents l4 As System.Windows.Forms.Label
+    Friend WithEvents l1 As System.Windows.Forms.Label
+    Friend WithEvents l3 As System.Windows.Forms.Label
+    Friend WithEvents l2 As System.Windows.Forms.Label
+    Friend WithEvents gb1 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ CUADRO MAGICO/CUADRO MAGICO/Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+    Dim a, b, c, d, e, f, g, h As Integer
+    Dim n As Integer = 0
+    Dim nums(10) As System.Object
+    Dim i As Integer = 1
+    Private Sub gb1a(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles e1.Click, e2.Click, e3.Click, e4.Click, e5.Click, e6.Click, e7.Click, e8.Click, e9.Click
+        If sender.text = "" And n > 0 Then
+            sender.text = n
+            nums(CInt(sender.text)).enabled = False
+
+        End If
+        If sender.text <> "" And n = 0 Then
+            nums(CInt(sender.text)).enabled = True
+            sender.text = ""
+        End If
+        If sender.text <> "" And n > 0 Then
+            nums(CInt(sender.text)).enabled = True
+            sender.text = n
+            nums(CInt(sender.text)).enabled = True
+            sender.text = n
+            nums(CInt(sender.text)).enabled = False
+        End If
+        n = 0
+        For Each obj In gb2.Controls
+            comprobar()
+        Next
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim i As Integer = 1
+        For Each obj In gb2.Controls
+            i = CInt(obj.text)
+            nums(i) = obj
+
+        Next
+    End Sub
+
+    Private Sub gb2_Enter_2(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles l1.Click, l2.Click, l3.Click, l4.Click, l5.Click, l6.Click, l7.Click, l8.Click, l9.Click
+        n = sender.text
+    End Sub
+    Public Sub comprobar()
+        a = CInt("e1.Text + e2.Text + e3.Text")
+        b = CInt("e4.Text + e5.Text + e6.Text")
+        c = CInt("e7.Text + e8.Text + e9.Text")
+        d = CInt("e1.Text + e4.Text + e7.Text")
+        e = CInt("e2.Text + e5.Text + e8.Text")
+        f = CInt("e3.Text + e6.Text + e9.Text")
+        g = CInt("e1.Text + e5.Text + e9.Text")
+        h = CInt("e3.Text + e5.Text + e7.Text")
+        If a = b And a = c And a = d And a = e And a = f And a = g And a = h Then
+            MsgBox("win")
+        Else
+            MsgBox("vuelvelo a intentar")
+        End If
+    End Sub
+
+    Private Sub gb2_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles gb2.Enter
+
+    End Sub
+
+    Private Sub e7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles e7.Click
+
+    End Sub
+
+    Private Sub e1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles e1.Click
+
+    End Sub
+
+    Private Sub gb1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles gb1.Enter
+
+    End Sub
+
+    Private Sub l4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles l4.Click
+
+    End Sub
+
+    Private Sub e2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles e2.Click
+
+    End Sub
+End Class
