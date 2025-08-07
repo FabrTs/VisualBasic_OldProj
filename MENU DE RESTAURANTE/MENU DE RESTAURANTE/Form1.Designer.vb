cad661fd64580a8cdf786bfa065aba8dad921be0
Fabricio Torres <99437781+FabrTs@users.noreply.github.com>
2025-08-07 12:08:15 -0600

    Add files via upload

---
+++ MENU DE RESTAURANTE/MENU DE RESTAURANTE/Form1.Designer.vb
@@ -0,0 +1,422 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.b16 = New System.Windows.Forms.Button
+        Me.b15 = New System.Windows.Forms.Button
+        Me.b14 = New System.Windows.Forms.Button
+        Me.b13 = New System.Windows.Forms.Button
+        Me.b8 = New System.Windows.Forms.Button
+        Me.b9 = New System.Windows.Forms.Button
+        Me.b10 = New System.Windows.Forms.Button
+        Me.b11 = New System.Windows.Forms.Button
+        Me.b12 = New System.Windows.Forms.Button
+        Me.b2 = New System.Windows.Forms.Button
+        Me.b3 = New System.Windows.Forms.Button
+        Me.b4 = New System.Windows.Forms.Button
+        Me.b5 = New System.Windows.Forms.Button
+        Me.b6 = New System.Windows.Forms.Button
+        Me.b7 = New System.Windows.Forms.Button
+        Me.b1 = New System.Windows.Forms.Button
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.t4 = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.li1 = New System.Windows.Forms.ListBox
+        Me.bt2 = New System.Windows.Forms.Button
+        Me.bt1 = New System.Windows.Forms.Button
+        Me.t3 = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.t2 = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.t1 = New System.Windows.Forms.TextBox
+        Me.li2 = New System.Windows.Forms.ListBox
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.b16)
+        Me.GroupBox1.Controls.Add(Me.b15)
+        Me.GroupBox1.Controls.Add(Me.b14)
+        Me.GroupBox1.Controls.Add(Me.b13)
+        Me.GroupBox1.Controls.Add(Me.b8)
+        Me.GroupBox1.Controls.Add(Me.b9)
+        Me.GroupBox1.Controls.Add(Me.b10)
+        Me.GroupBox1.Controls.Add(Me.b11)
+        Me.GroupBox1.Controls.Add(Me.b12)
+        Me.GroupBox1.Controls.Add(Me.b2)
+        Me.GroupBox1.Controls.Add(Me.b3)
+        Me.GroupBox1.Controls.Add(Me.b4)
+        Me.GroupBox1.Controls.Add(Me.b5)
+        Me.GroupBox1.Controls.Add(Me.b6)
+        Me.GroupBox1.Controls.Add(Me.b7)
+        Me.GroupBox1.Controls.Add(Me.b1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 6)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(346, 308)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "PRODUCTOS"
+        '
+        'b16
+        '
+        Me.b16.Location = New System.Drawing.Point(237, 240)
+        Me.b16.Name = "b16"
+        Me.b16.Size = New System.Drawing.Size(88, 61)
+        Me.b16.TabIndex = 8
+        Me.b16.Tag = "24"
+        Me.b16.Text = "ORDEN DE CEBOLLAS"
+        Me.b16.UseVisualStyleBackColor = True
+        '
+        'b15
+        '
+        Me.b15.Location = New System.Drawing.Point(154, 241)
+        Me.b15.Name = "b15"
+        Me.b15.Size = New System.Drawing.Size(78, 61)
+        Me.b15.TabIndex = 9
+        Me.b15.Tag = "15"
+        Me.b15.Text = "ORDEN DE TORTILLAS"
+        Me.b15.UseVisualStyleBackColor = True
+        '
+        'b14
+        '
+        Me.b14.Location = New System.Drawing.Point(80, 241)
+        Me.b14.Name = "b14"
+        Me.b14.Size = New System.Drawing.Size(68, 61)
+        Me.b14.TabIndex = 10
+        Me.b14.Tag = "17"
+        Me.b14.Text = "HELADO FRESA"
+        Me.b14.UseVisualStyleBackColor = True
+        '
+        'b13
+        '
+        Me.b13.Location = New System.Drawing.Point(6, 240)
+        Me.b13.Name = "b13"
+        Me.b13.Size = New System.Drawing.Size(68, 61)
+        Me.b13.TabIndex = 11
+        Me.b13.Tag = "19"
+        Me.b13.Text = "HELADO VAINILLA"
+        Me.b13.UseVisualStyleBackColor = True
+        '
+        'b8
+        '
+        Me.b8.Location = New System.Drawing.Point(238, 95)
+        Me.b8.Name = "b8"
+        Me.b8.Size = New System.Drawing.Size(87, 61)
+        Me.b8.TabIndex = 11
+        Me.b8.Tag = "12"
+        Me.b8.Text = "AGUA GRANDE"
+        Me.b8.UseVisualStyleBackColor = True
+        '
+        'b9
+        '
+        Me.b9.Location = New System.Drawing.Point(6, 173)
+        Me.b9.Name = "b9"
+        Me.b9.Size = New System.Drawing.Size(68, 61)
+        Me.b9.TabIndex = 10
+        Me.b9.Tag = "12"
+        Me.b9.Text = "FRIJOLESCHARROS"
+        Me.b9.UseVisualStyleBackColor = True
+        '
+        'b10
+        '
+        Me.b10.Location = New System.Drawing.Point(80, 173)
+        Me.b10.Name = "b10"
+        Me.b10.Size = New System.Drawing.Size(68, 61)
+        Me.b10.TabIndex = 9
+        Me.b10.Tag = "25"
+        Me.b10.Text = "REBANADA DE FLAN"
+        Me.b10.UseVisualStyleBackColor = True
+        '
+        'b11
+        '
+        Me.b11.Location = New System.Drawing.Point(154, 173)
+        Me.b11.Name = "b11"
+        Me.b11.Size = New System.Drawing.Size(78, 61)
+        Me.b11.TabIndex = 8
+        Me.b11.Tag = "28"
+        Me.b11.Text = "REBANADA DE PAY DE QUESO"
+        Me.b11.UseVisualStyleBackColor = True
+        '
+        'b12
+        '
+        Me.b12.Location = New System.Drawing.Point(238, 173)
+        Me.b12.Name = "b12"
+        Me.b12.Size = New System.Drawing.Size(88, 61)
+        Me.b12.TabIndex = 7
+        Me.b12.Tag = "18"
+        Me.b12.Text = "HELADO NAPOLITANO"
+        Me.b12.UseVisualStyleBackColor = True
+        '
+        'b2
+        '
+        Me.b2.Location = New System.Drawing.Point(80, 28)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(68, 61)
+        Me.b2.TabIndex = 6
+        Me.b2.Tag = "40"
+        Me.b2.Text = "HAMBURGUESA JUMBO"
+        Me.b2.UseVisualStyleBackColor = True
+        '
+        'b3
+        '
+        Me.b3.Location = New System.Drawing.Point(154, 28)
+        Me.b3.Name = "b3"
+        Me.b3.Size = New System.Drawing.Size(78, 61)
+        Me.b3.TabIndex = 5
+        Me.b3.Tag = "10"
+        Me.b3.Text = "REFRESCO GRANDE"
+        Me.b3.UseVisualStyleBackColor = True
+        '
+        'b4
+        '
+        Me.b4.Location = New System.Drawing.Point(238, 28)
+        Me.b4.Name = "b4"
+        Me.b4.Size = New System.Drawing.Size(87, 61)
+        Me.b4.TabIndex = 4
+        Me.b4.Tag = "6"
+        Me.b4.Text = "REFRESCO CHICO"
+        Me.b4.UseVisualStyleBackColor = True
+        '
+        'b5
+        '
+        Me.b5.Location = New System.Drawing.Point(6, 95)
+        Me.b5.Name = "b5"
+        Me.b5.Size = New System.Drawing.Size(68, 61)
+        Me.b5.TabIndex = 3
+        Me.b5.Tag = "32"
+        Me.b5.Text = "PAPAS CHICAS"
+        Me.b5.UseVisualStyleBackColor = True
+        '
+        'b6
+        '
+        Me.b6.Location = New System.Drawing.Point(80, 95)
+        Me.b6.Name = "b6"
+        Me.b6.Size = New System.Drawing.Size(68, 61)
+        Me.b6.TabIndex = 2
+        Me.b6.Tag = "50"
+        Me.b6.Text = "PAPAS GRANDES"
+        Me.b6.UseVisualStyleBackColor = True
+        '
+        'b7
+        '
+        Me.b7.Location = New System.Drawing.Point(154, 95)
+        Me.b7.Name = "b7"
+        Me.b7.Size = New System.Drawing.Size(78, 61)
+        Me.b7.TabIndex = 1
+        Me.b7.Tag = "8"
+        Me.b7.Text = "AGUA CHICA"
+        Me.b7.UseVisualStyleBackColor = True
+        '
+        'b1
+        '
+        Me.b1.Location = New System.Drawing.Point(6, 28)
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(68, 61)
+        Me.b1.TabIndex = 0
+        Me.b1.Tag = "20"
+        Me.b1.Text = "HAMBURGUESA "
+        Me.b1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.t4)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.li1)
+        Me.GroupBox2.Controls.Add(Me.bt2)
+        Me.GroupBox2.Controls.Add(Me.bt1)
+        Me.GroupBox2.Controls.Add(Me.t3)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.t2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.t1)
+        Me.GroupBox2.Location = New System.Drawing.Point(393, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(320, 302)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "COMANDA"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(20, 268)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "NVA. VENTA"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        't4
+        '
+        Me.t4.Location = New System.Drawing.Point(187, 268)
+        Me.t4.Name = "t4"
+        Me.t4.Size = New System.Drawing.Size(88, 20)
+        Me.t4.TabIndex = 4
+        Me.t4.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(137, 272)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "TOTAL:"
+        '
+        'li1
+        '
+        Me.li1.FormattingEnabled = True
+        Me.li1.Location = New System.Drawing.Point(20, 156)
+        Me.li1.Name = "li1"
+        Me.li1.Size = New System.Drawing.Size(273, 95)
+        Me.li1.TabIndex = 6
+        '
+        'bt2
+        '
+        Me.bt2.Location = New System.Drawing.Point(201, 124)
+        Me.bt2.Name = "bt2"
+        Me.bt2.Size = New System.Drawing.Size(75, 23)
+        Me.bt2.TabIndex = 5
+        Me.bt2.Text = "ELIMINAR"
+        Me.bt2.UseVisualStyleBackColor = True
+        '
+        'bt1
+        '
+        Me.bt1.Location = New System.Drawing.Point(44, 124)
+        Me.bt1.Name = "bt1"
+        Me.bt1.Size = New System.Drawing.Size(75, 23)
+        Me.bt1.TabIndex = 4
+        Me.bt1.Text = "AGREGAR"
+        Me.bt1.UseVisualStyleBackColor = True
+        '
+        't3
+        '
+        Me.t3.Location = New System.Drawing.Point(94, 92)
+        Me.t3.Name = "t3"
+        Me.t3.Size = New System.Drawing.Size(209, 20)
+        Me.t3.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "CANTIDAD:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "PRECIO:"
+        '
+        't2
+        '
+        Me.t2.Location = New System.Drawing.Point(94, 61)
+        Me.t2.Name = "t2"
+        Me.t2.Size = New System.Drawing.Size(209, 20)
+        Me.t2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "PRODUCTO:"
+        '
+        't1
+        '
+        Me.t1.Location = New System.Drawing.Point(94, 31)
+        Me.t1.Name = "t1"
+        Me.t1.Size = New System.Drawing.Size(209, 20)
+        Me.t1.TabIndex = 0
+        '
+        'li2
+        '
+        Me.li2.FormattingEnabled = True
+        Me.li2.Location = New System.Drawing.Point(769, 101)
+        Me.li2.Name = "li2"
+        Me.li2.Size = New System.Drawing.Size(120, 95)
+        Me.li2.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(887, 326)
+        Me.Controls.Add(Me.li2)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "MENU"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents b8 As System.Windows.Forms.Button
+    Friend WithEvents b9 As System.Windows.Forms.Button
+    Friend WithEvents b10 As System.Windows.Forms.Button
+    Friend WithEvents b11 As System.Windows.Forms.Button
+    Friend WithEvents b12 As System.Windows.Forms.Button
+    Friend WithEvents b2 As System.Windows.Forms.Button
+    Friend WithEvents b3 As System.Windows.Forms.Button
+    Friend WithEvents b4 As System.Windows.Forms.Button
+    Friend WithEvents b5 As System.Windows.Forms.Button
+    Friend WithEvents b6 As System.Windows.Forms.Button
+    Friend WithEvents b7 As System.Windows.Forms.Button
+    Friend WithEvents b1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents b16 As System.Windows.Forms.Button
+    Friend WithEvents b15 As System.Windows.Forms.Button
+    Friend WithEvents b14 As System.Windows.Forms.Button
+    Friend WithEvents b13 As System.Windows.Forms.Button
+    Friend WithEvents bt2 As System.Windows.Forms.Button
+    Friend WithEvents bt1 As System.Windows.Forms.Button
+    Friend WithEvents t3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents t2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents t1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents t4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents li1 As System.Windows.Forms.ListBox
+    Friend WithEvents li2 As System.Windows.Forms.ListBox
+
+End Class
---
+++ MENU DE RESTAURANTE/MENU DE RESTAURANTE/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Dim a As Double
+    Dim i As Integer
+    Dim b As Double
+
+
+    Private Sub b1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles b1.Click, b2.Click, b3.Click, b4.Click, b5.Click, b6.Click, b7.Click, b8.Click, b9.Click, b10.Click, b11.Click, b12.Click, b13.Click, b14.Click, b15.Click, b16.Click
+        t1.Text = sender.text
+        t2.Text = sender.tag
+        t3.Focus()
+    End Sub
+
+    Private Sub bt1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt1.Click
+        a = CDbl(t2.Text) * CDbl(t3.Text)
+        t4.Text = a + CDbl(t4.Text)
+        li1.Items.Add(t3.Text + t1.Text + CStr(a))
+        t3.Text = ""
+        li2.Items.Add(CStr(a))
+    End Sub
+    Private Sub resta(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt2.Click
+        i = li1.SelectedIndex
+        b = li2.Items(i)
+        li1.Items.RemoveAt(i)
+        li2.Items.RemoveAt(i)
+        t4.Text = CDbl(t4.Text) - b
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        a = 0
+        i = 0
+        b = 0
+        t1.Text = ""
+        t2.Text = ""
+        t3.Text = ""
+        t4.Text = 0
+        li1.Items.Clear()
+        li2.Items.Clear()
+    End Sub
+
+    Private Sub li1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles li1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub t3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles t3.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+End Class
