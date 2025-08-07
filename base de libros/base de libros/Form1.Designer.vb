5f8461676d75e1945408a82fce3418d70ece4eb7
Fabricio Torres <99437781+FabrTs@users.noreply.github.com>
2025-08-07 12:01:07 -0600

    Add files via upload

---
+++ base de libros/base de libros/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.t1 = New System.Windows.Forms.TextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.label1 = New System.Windows.Forms.Label
+        Me.dvg1 = New System.Windows.Forms.DataGridView
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.t2 = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.t3 = New System.Windows.Forms.TextBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.t5 = New System.Windows.Forms.TextBox
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.t4 = New System.Windows.Forms.TextBox
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        CType(Me.dvg1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        't1
+        '
+        Me.t1.Location = New System.Drawing.Point(167, 34)
+        Me.t1.Name = "t1"
+        Me.t1.Size = New System.Drawing.Size(138, 20)
+        Me.t1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(311, 32)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "buscar libro"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(111, 41)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(26, 13)
+        Me.label1.TabIndex = 2
+        Me.label1.Text = "isbn"
+        '
+        'dvg1
+        '
+        Me.dvg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dvg1.Location = New System.Drawing.Point(12, 180)
+        Me.dvg1.Name = "dvg1"
+        Me.dvg1.Size = New System.Drawing.Size(558, 150)
+        Me.dvg1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(111, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "titulo"
+        '
+        't2
+        '
+        Me.t2.Location = New System.Drawing.Point(167, 60)
+        Me.t2.Name = "t2"
+        Me.t2.Size = New System.Drawing.Size(138, 20)
+        Me.t2.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(111, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "autor"
+        '
+        't3
+        '
+        Me.t3.Location = New System.Drawing.Point(167, 86)
+        Me.t3.Name = "t3"
+        Me.t3.Size = New System.Drawing.Size(138, 20)
+        Me.t3.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(111, 144)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "nombre"
+        '
+        't5
+        '
+        Me.t5.Location = New System.Drawing.Point(167, 137)
+        Me.t5.Name = "t5"
+        Me.t5.Size = New System.Drawing.Size(138, 20)
+        Me.t5.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(111, 118)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(15, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "id"
+        '
+        't4
+        '
+        Me.t4.Location = New System.Drawing.Point(167, 111)
+        Me.t4.Name = "t4"
+        Me.t4.Size = New System.Drawing.Size(138, 20)
+        Me.t4.TabIndex = 8
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(311, 109)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "buscar socio"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(310, 154)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "agregar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(582, 352)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.t5)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.t4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.t3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.t2)
+        Me.Controls.Add(Me.dvg1)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.t1)
+        Me.Name = "Form1"
+        Me.Text = "autor"
+        CType(Me.dvg1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents t1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents label1 As System.Windows.Forms.Label
+    Friend WithEvents dvg1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents t2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents t3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents t5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents t4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ base de libros/base de libros/Form1.vb
@@ -0,0 +1,93 @@
+﻿Public Class Form1
+    Dim conexion As New OleDb.OleDbConnection
+    Dim ds As New DataSet
+    Dim dal, das, dap As OleDb.OleDbDataAdapter
+    Dim cb1, cb2 As OleDb.OleDbCommandBuilder
+    Dim dr As DataRow
+    Dim dv As New DataView
+    Dim pos As Integer
+    Dim valor As String
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Try
+            conexion.Open()
+            dal.Update(ds, "libros")
+            dap.Update(ds, "prestamos")
+            conexion.Close()
+        Catch ex As Exception
+            MsgBox("No se pudieron actualizar los datos")
+        End Try
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim provedor, ruta As String
+        provedor = "provider=microsoft.ace.oledb.12.0;"
+        ruta = "data source=" + Application.StartupPath + "\libros.accdb"
+        conexion.ConnectionString = provedor + ruta
+        Try
+            conexion.Open()
+            das = New OleDb.OleDbDataAdapter("select * from socios", conexion)
+            dal = New OleDb.OleDbDataAdapter("select * from libros", conexion)
+            cb1 = New OleDb.OleDbCommandBuilder(dal)
+            dap = New OleDb.OleDbDataAdapter("select *from prestamos", conexion)
+            cb2 = New OleDb.OleDbCommandBuilder(dap)
+            das.Fill(ds, "socios")
+            dal.Fill(ds, "libros")
+            dap.Fill(ds, "prestamos")
+            conexion.Close()
+            dv.Table = ds.Tables("prestamos")
+            dv.RowFilter = "id=''"
+            dvg1.DataSource = dv
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        For j = 0 To ds.Tables("libros").Rows.Count - 1
+            If t1.Text = ds.Tables("libros").Rows(j).Item(0) Then
+                't1.Text = ds.Tables("libros").Rows(j).Item(0)
+                t2.Text = ds.Tables("libros").Rows(j).Item(1)
+                t3.Text = ds.Tables("libros").Rows(j).Item(2)
+                valor = ds.Tables("libros").Rows(j).Item(3)
+                pos = j
+                Exit For
+            End If
+        Next
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles t2.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        For l = 0 To ds.Tables("socios").Rows.Count - 1
+            If t4.Text = ds.Tables("socios").Rows(l).Item(0) Then
+                t4.Text = ds.Tables("socios").Rows(l).Item(0)
+                t5.Text = ds.Tables("socios").Rows(l).Item(1)
+                dv.RowFilter = "id='" + t4.Text + "'"
+                'dv.rowfilter="id='s001'"
+            End If
+        Next
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim fi, ff As Date
+        fi = Today
+        ff = Today
+        dr = ds.Tables("prestamos").NewRow
+        dr.Item(0) = t1.Text + t4.Text + fi
+        dr.Item(1) = t4.Text
+        dr.Item(2) = t1.Text
+        dr.Item(3) = t5.Text
+        dr.Item(4) = fi
+        dr.Item(5) = ff
+        dr.Item(6) = valor
+        dr.Item(7) = t2.Text
+        ds.Tables("prestamos").Rows.Add(dr)
+        ds.Tables("libros").Rows(pos).Item(3) = True
+
+    End Sub
+End Class
